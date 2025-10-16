8db3f0bf50e8b3918fc6ab77cda984f217c42c14
Kat <160546993+FlyingCuda@users.noreply.github.com>
2025-10-17 00:18:42 +0300

    directories added

---
+++ src/test/java/com/example/mypetshop/MyPetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
